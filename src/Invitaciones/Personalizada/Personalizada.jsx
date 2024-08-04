50351bc5a2c375fbc8f1710a6c864a7280479c56
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2024-08-04 14:12:57 +0200

    personlaizada

---
+++ src/Invitaciones/Personalizada/Personalizada.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+
+
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`
+const Image = styled.img`
+  width: 100%;
+  max-width: 430px;
+  height: auto;
+`
+const ImageSave = styled.img`
+  width: 90%;
+  max-width: 400px;
+  height: auto;
+  margin-bottom: 10px;
+`
+
+const ImageBride = styled.img`
+  width: 100%;
+  max-width: 300px;
+  height: auto;
+  margin-top: 20px;
+  border-radius: 15px;
+  margin-bottom: 20px;
+`
+
+const DivDate = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  gap: 10px;
+`
+
+const DateSpan = styled.span`
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 50px;
+  color: #454545;
+  width: 75px;
+  text-align: center;
+`
+const DateSpanWord = styled.span`
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 20px;
+  color: #454545;
+  width: 75px;
+  text-align: center;
+`
+
+function Personalizada() {
+
+}
+
+export default Personalizada
